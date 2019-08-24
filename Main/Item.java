35af44515451510327fed5a3ff5b44bec09a7143
Daniel Whiteman <danielwhiteman718@gmail.com>
2019-08-24 22:12:58 +0100

    Adding getters, setters and instance vars to item.java

--- Main/Item.java
+++ Main/Item.java
@@ -2,4 +2,28 @@ package Main;
 
 public class Item{
 	
+	private String name;
+	private double price;
+	
+	public Item(String n, double p) {
+		name = n;
+		price = p;
+	}
+	
+	public void setName(String n) {
+		name = n;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setPrice(double p) {
+		price = p;
+	}
+	
+	public double getPrice(){
+		return price;
+	}
+	
 }
