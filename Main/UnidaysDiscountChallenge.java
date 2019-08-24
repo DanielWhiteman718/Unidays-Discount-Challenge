c11352ff3d1141944040d631bdaba4cac51360fa
Daniel Whiteman <danielwhiteman718@gmail.com>
2019-08-24 22:14:19 +0100

    Adding same to UnidaysDiscountChallenge.java

--- Main/UnidaysDiscountChallenge.java
+++ Main/UnidaysDiscountChallenge.java
@@ -4,4 +4,18 @@ import java.util.ArrayList;
 
 public class UnidaysDiscountChallenge{
 	
+	private ArrayList<Item> basket;
+	private double totalPrice;
+	
+	public UnidaysDiscountChallenge(){
+		basket = new ArrayList<Item>();
+	}
+	
+	public static void main(String[] args) {
+		Item a = new Item("A", 5.6);
+	}
+	
+	public void AddToBasket(Item item) {
+		basket.add(item);
+	}
 }
